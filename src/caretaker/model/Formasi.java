ab69a4b17ed84002a9db045a2d9ef0a385a966b3
Nino Fachrurozy <48285366+nfach98@users.noreply.github.com>
2019-12-15 23:37:51 +0700

    GA 80%

--- src/caretaker/model/Formasi.java
+++ src/caretaker/model/Formasi.java
@@ -12,10 +12,22 @@ public class Formasi {
         this.formasi = formasi;
         
         switch(formasi){
+            case "4-3-3":
+                posisi = new ArrayList<>(Arrays.asList("GK","LB","CB","CB","RB","CM","CM","CM","LW","RW","ST")); break;
+            case "3-4-3":
+                posisi = new ArrayList<>(Arrays.asList("GK","CB","CB","CB","LM","CM","CM","RM","LW","RW","ST")); break;
+            case "3-4-2-1":
+                posisi = new ArrayList<>(Arrays.asList("GK","CB","CB","CB","LM","CM","CM","RM","LF","RF","ST")); break;
+            case "4-2-3-1":
+                posisi = new ArrayList<>(Arrays.asList("GK","LB","CB","CB","RB","CDM","CDM","CAM","CAM","CAM","ST")); break;
             case "4-4-2":
                 posisi = new ArrayList<>(Arrays.asList("GK","LB","CB","CB","RB","LM","CM","CM","RM","ST","ST")); break;
-            case "4-3-3":
-                posisi = new ArrayList<>(Arrays.asList("GK","LB","CB","CB","RB","CM","CM","CM","LW","ST","RW")); break;    
+            case "4-5-1":
+                posisi = new ArrayList<>(Arrays.asList("GK","LB","CB","CB","RB","LM","CM","CM","CM","RM","ST")); break;
+            case "4-1-2-1-2":
+                posisi = new ArrayList<>(Arrays.asList("GK","CB","CB","CB","LM","CM","CM","RM","LF","RF","ST")); break;
+            case "5-2-1-2":
+                posisi = new ArrayList<>(Arrays.asList("GK","LWB","CB","CB","CB","RWB","CM","CM","CAM","ST","ST")); break;
         }
     }
 
